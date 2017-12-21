9d8ba1b335da0849616912cf8ac41343b358e3aa
Hyunjun Ko <zzoon@igalia.com>
2017-12-21 19:44:17 +0900

    msdk: implements GstMsdkContext.
    Makes GstMsdkContext to be a descendant of GstObject so that
    we could track the life-cycle of the session of the driver.
    
    Also replaces MsdkContext with this one.
    Keeps msdk_d3d.c alive for the future.

---
+++ sys/msdk/gstmsdkcontext.h
@@ -0,0 +1,91 @@
+/* GStreamer Intel MSDK plugin
+ * Copyright (c) 2017, Intel Corporation
+ * Copyright (c) 2017, Igalia S.L.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGDECE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef GST_MSDK_CONTEXT_H
+#define GST_MSDK_CONTEXT_H
+
+#include "msdk.h"
+#ifndef _WIN32
+#include <va/va.h>
+#endif
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_MSDK_CONTEXT \
+  (gst_msdk_context_get_type ())
+#define GST_MSDK_CONTEXT(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), GST_TYPE_MSDK_CONTEXT, \
+      GstMsdkContext))
+#define GST_MSDK_CONTEXT_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), GST_TYPE_MSDK_CONTEXT, \
+      GstMsdkContextClass))
+#define GST_IS_MSDK_CONTEXT(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GST_TYPE_MSDK_CONTEXT))
+#define GST_IS_MSDK_CONTEXT_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), GST_TYPE_MSDK_CONTEXT))
+#define GST_MSDK_CONTEXT_CAST(obj) ((GstMsdkContext*)(obj))
+
+typedef struct _GstMsdkContext GstMsdkContext;
+typedef struct _GstMsdkContextClass GstMsdkContextClass;
+typedef struct _GstMsdkContextPrivate GstMsdkContextPrivate;
+
+/*
+ * GstMsdkContext:
+ */
+struct _GstMsdkContext
+{
+  GstObject parent_instance;
+
+  GstMsdkContextPrivate *priv;
+};
+
+/*
+ * GstMsdkContextClass:
+ */
+struct _GstMsdkContextClass
+{
+  GstObjectClass parent_class;
+};
+
+GType gst_msdk_context_get_type (void);
+
+GstMsdkContext * gst_msdk_context_new (gboolean hardware);
+mfxSession gst_msdk_context_get_session (GstMsdkContext * context);
+
+#ifndef _WIN32
+VADisplay gst_msdk_context_get_va_display (GstMsdkContext * context);
+gint gst_msdk_context_get_fd (GstMsdkContext * context);
+#endif
+
+G_END_DECLS
+
+#endif /* GST_MSDK_CONTEXT_H */
